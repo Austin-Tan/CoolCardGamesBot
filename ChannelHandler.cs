351d9b81a7fe36beca2f187f81a256981ffb8d99
Austin Tan <austinwtan@gmail.com>
2020-11-17 03:15:04 -0800

    Reaction handlers

---
+++ ChannelHandler.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class ChannelHandler
+    {
+        // We could choose to go to a higher interface like ISocketMessageChannel
+        // but I think we always want this more specific scope.
+        SocketTextChannel channel;
+
+        // the keys are the ulongs - messageIds of relevant messages
+        private Dictionary<ulong, int> messages;
+
+        public ChannelHandler(ISocketMessageChannel newChannel)
+        {
+            var castedChannel = newChannel as SocketTextChannel;
+            if (castedChannel == null)
+            {
+                throw new InvalidCastException(
+                    "Could not cast passed channel " + 
+                    "to SocketTextChannel");
+            }
+            channel = castedChannel;
+            messages = new Dictionary<ulong, int>();
+        }
+
+
+        public void ProcessReaction(ulong messageId, string emote, ulong userId, bool adding) {
+
+        }
+
+
+        // super clunky to hardcode like this, set up a state for the game
+        //if (msg.Author.Id == _client.CurrentUser.Id && msg.Content == "Do you really want to kill this game?\n" +
+        //        "🗑️ to confirm deletion. 👎 to keep playing.") {
+        //    Console.WriteLine("On a kill message");
+        //    if (reaction.Emote.Name == new Emoji("🗑️").Name || reaction.Emote.Name == new Emoji("🗑").Name) {
+        //        channel.SendMessageAsync($"Game of ***{activeGames[channel.Id].activeGame.Name}*** deleted 🗑️");
+        //        activeGames.Remove(channel.Id);
+        //    } else {
+        //        channel.SendMessageAsync("We'll keep playing ***{activeGames[channel.Id].Name}*** 😃");
+        //        channel.DeleteMessageAsync(msg.Id);
+        //    }
+        //    channel.DeleteMessageAsync(msg.Id);
+        //} else {
+        //    Console.WriteLine("Hitting the map now with react by " + reaction.User.Value.Username);
+        //    activeGames[channel.Id].messageMap[msg.Id](msg, reaction.User.Value);
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,30 +6,24 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace DiscordBot
-{
-    public enum DiscardGames
-    {
+namespace DiscordBot {
+    public enum DiscardGames {
         NoThanks,
-        IncanGold,
-        SixNimmt,
         NOTFOUND
     }
 
-    class Program
-    {
+    class Program {
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
 
-        // Dictionary mapping channels to active games
-        public Dictionary<ulong, GameHandler> activeGames;
+        // Dictionary mapping IDs to active channels
+        public Dictionary<ulong, ChannelHandler> activeChannels;
 
-        public async Task MainAsync()
-        {
+        public async Task MainAsync() {
 
-            DiscordSocketConfig config = new DiscordSocketConfig { MessageCacheSize = 64, GuildSubscriptions = false};
+            DiscordSocketConfig config = new DiscordSocketConfig { MessageCacheSize = 64, GuildSubscriptions = false };
             // When Discord.Net.WebSocket receives a stable update to include GatewayIntents, add here
             // and unsub from typing, etc. instead of guildsubscriptions
 
@@ -39,19 +33,11 @@ namespace DiscordBot
             _client.ReactionRemoved += ReactionRemover;
             _client.Log += Log;
 
-            //  You can assign your bot token to a string, and pass that in to connect.
-
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
-            // Copy!Paste your token in Environment Variables under User.
-            // Can select System!wide Env Variables instead of User, just change the 2nd Param for targeting.
+            // Copy-Paste your token in Environment Variables under User.
+            // Can select System-wide Env Variables instead of User, just change the 2nd Param for targeting.
             var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN", EnvironmentVariableTarget.User);
-            // Console.WriteLine("token is :" + token);
 
-            activeGames = new Dictionary<ulong, GameHandler>();
+            activeChannels = new Dictionary<ulong, ChannelHandler>();
 
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
@@ -62,19 +48,69 @@ namespace DiscordBot
             await Task.Delay(-1);
         }
 
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
+        private Task Log(LogMessage msg) {
             return Task.CompletedTask;
         }
 
-        private Task RouteMessage(string command, SocketMessage message)
-        {
+        private ChannelHandler GetChannelHandler(ISocketMessageChannel channel) {
+            ulong id = channel.Id;
+            if (!activeChannels.ContainsKey(id)) {
+                activeChannels.Add(id, new ChannelHandler(channel));
+            }
+            return activeChannels[id];
+        }
+
+        private Task ReactionRouter( ISocketMessageChannel channel, SocketReaction reaction, bool adding) {
+            // This is a reaction we added ourselves. Ignore it.
+            if (reaction.UserId == _client.CurrentUser.Id) {
+                return Task.CompletedTask;
+            }
+
+            ulong msgId = reaction.MessageId;
+
+            //IUserMessage msg;
+            //if (reaction.Message.IsSpecified) {
+            //    msg = reaction.Message.Value;
+            //} else {
+            //    var preCast = channel.GetCachedMessage(reaction.MessageId);
+            //    if (preCast == null) {
+            //        throw new KeyNotFoundException("Couldn't find target message in the cache.");
+            //    }
+            //    var castedMsg = preCast as IUserMessage;
+            //    msg = castedMsg ?? throw new InvalidCastException("Could not cast SocketMessage to IUSerMessage");
+            //}
+
+            ChannelHandler handler = GetChannelHandler(channel);
+
+            handler.ProcessReaction(msgId, reaction.Emote.Name, reaction.UserId, adding);
+
+            return Task.CompletedTask;
+        }
+
+        private Task ReactionAdder(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel channel, SocketReaction reaction) {
+            return ReactionRouter(channel, reaction, true);
+        }
+
+        private Task ReactionRemover(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel channel, SocketReaction reaction) {
+            return ReactionRouter(channel, reaction, false);
+        }
+
+        private Task MessageHandler(SocketMessage message) {
+            if (!message.Content.StartsWith('!')) {
+                return Task.CompletedTask;
+            }
+            if (message.Author.IsBot) {
+                return Task.CompletedTask;
+            }
+
+            return RouteMessage(message.Content.Substring(1).Trim(), message);
+        }
+
+        private Task RouteMessage(string command, SocketMessage message) {
             string author = message.Author.Username;
             Console.WriteLine($"{author} asked:\n{command}");
 
-            if (command.ToLower().Trim() == "test")
-            {
+            if (command.ToLower().Trim() == "test") {
                 string imageURL = "https://www.pojo.com/wp-content/uploads/2018/01/no-thanks-box-art.jpg";
                 EmbedBuilder builder = new EmbedBuilder { ImageUrl = $"attachment://{imageURL}" };
                 Embed emb = builder.Build();
@@ -86,10 +122,8 @@ namespace DiscordBot
                 builder.WithImageUrl($"{imageURL}");
                 message.Channel.SendMessageAsync("msg2");
                 message.Channel.SendMessageAsync(null, false, builder.Build());
-            } else if (command.ToLower().Trim() == "help")
-            {
-                EmbedBuilder builder = new EmbedBuilder
-                {
+            } else if (command.ToLower().Trim() == "help") {
+                EmbedBuilder builder = new EmbedBuilder {
                     Title = "HELP - Cool Card Games Bot",
                     Description = "List of commands:",
                 };
@@ -103,8 +137,7 @@ namespace DiscordBot
                 Embed msg = builder.Build();
 
                 message.Channel.SendMessageAsync(null, false, msg);
-            } else if (command.ToLower().Trim() == "bugsnax")
-            {
+            } else if (command.ToLower().Trim() == "bugsnax") {
                 message.Channel.SendMessageAsync("it's bugsnax, " + message.Author.Mention);
                 message.AddReactionAsync(new Emoji("\U0001f495"));
 
@@ -112,29 +145,24 @@ namespace DiscordBot
 
 
                 sent.Result.AddReactionsAsync(new IEmote[] { new Emoji("🇧"), new Emoji("🇺"), new Emoji("🇬") });
-            } else if (command.Split(' ')[0].ToLower() == "echo")
-            {
+            } else if (command.Split(' ')[0].ToLower() == "echo") {
                 string toSend = command.Substring(command.ToLower().IndexOf("echo ") + "echo ".Length);
                 message.Channel.SendMessageAsync(toSend + "\n- " + message.Author.Mention);
             } else if (command.ToLower().Trim().StartsWith("kill")) {
-                if (activeGames.ContainsKey(message.Channel.Id))
-                {
+                if (activeGames.ContainsKey(message.Channel.Id)) {
                     Task<RestUserMessage> sent = message.Channel.SendMessageAsync("Do you really want to kill this game?\n" +
                         "🗑️ to confirm deletion. 👎 to keep playing.");
                     sent.Result.AddReactionsAsync(new IEmote[] { new Emoji("👎"), new Emoji("🗑️") });
-                } else
-                {
+                } else {
                     message.Channel.SendMessageAsync("Could not find a game running in this channel to kill!");
                 }
-            } else if (command.Split(' ')[0].ToLower() == "play")
-            {
+            } else if (command.Split(' ')[0].ToLower() == "play") {
                 // find right game/validate string
                 string gameName = command.Substring(command.ToLower().IndexOf("play ") + "play ".Length);
                 DiscardGames newGame = findGameFromString(gameName);
 
                 // make sure it's an actually implemented game
-                if (newGame == DiscardGames.NOTFOUND)
-                {
+                if (newGame == DiscardGames.NOTFOUND) {
                     message.Channel.SendMessageAsync("Sorry, I couldn't find " + gameName + " 😓 Please try again!");
                     return Task.CompletedTask;
                 }
@@ -147,18 +175,15 @@ namespace DiscordBot
 
 
                 // check that the channel doesn't already have a game running
-                if (activeGames.ContainsKey(message.Channel.Id))
-                {
+                if (activeGames.ContainsKey(message.Channel.Id)) {
                     message.Channel.SendMessageAsync("Looks like this channel is already running a game! 😓 " +
                         "Please finish the current one, or -play in a different text channel!");
                     return Task.CompletedTask;
                 }
 
                 launchGame(newGame, message.Channel, message.Author);
-            } else
-            {
-                if (message.Channel.GetType() == typeof(SocketDMChannel))
-                {
+            } else {
+                if (message.Channel.GetType() == typeof(SocketDMChannel)) {
                     message.Channel.SendMessageAsync("Sorry I didn't seem to understand your message!" +
                         " 😓 Try !help for assistance.");
                 }
@@ -168,143 +193,30 @@ namespace DiscordBot
         }
 
         // Creates and starts a new game of type enumName in passed Discord channel.
-        public async void launchGame(DiscardGames enumName, ISocketMessageChannel channel, SocketUser author)
-        {
-            if (enumName == DiscardGames.NoThanks)
-            {
+        public async void launchGame(DiscardGames enumName, ISocketMessageChannel channel, SocketUser author) {
+            if (enumName == DiscardGames.NoThanks) {
                 activeGames.Add(channel.Id, new GameHandler(new NoThanksGame()));
                 await channel.SendMessageAsync("Starting a game of No Thanks!", false, activeGames[channel.Id].activeGame.Blurb());
-            } else if (enumName == DiscardGames.IncanGold)
-            {
-                activeGames.Add(channel.Id, new GameHandler(new IncanGoldGame()));
-                await channel.SendMessageAsync("Starting a game of Incan Gold!", false, activeGames[channel.Id].activeGame.Blurb());
-            } else if (enumName == DiscardGames.SixNimmt)
-            {
-                activeGames.Add(channel.Id, new GameHandler(new SixNimmtGame()));
-                await channel.SendMessageAsync("Starting a game of 6 Nimmt!", false, activeGames[channel.Id].activeGame.Blurb());
-            } else
-            {
+            } else {
                 await channel.SendMessageAsync("this is bad i'm in an else case i shouldn't ever be in.");
                 return;
             }
 
             GameHandler handler = activeGames[channel.Id];
-            
+
             var reactTo = await channel.SendMessageAsync("React to this message with 👍 to join!\nCurrent Players: ");
             await reactTo.AddReactionAsync(new Emoji("👍"));
 
             handler.messageMap.Add(reactTo.Id, handler.JoinGameMessage);
         }
 
-        //public static string enumToFormalName(DiscardGames enumName) {
-        //    if (enumName == DiscardGames.IncanGold)
-        //    {
-        //        return "Incan Gold";
-        //    } else if (enumName == DiscardGames.NoThanks)
-        //    {
-        //        return "No Thanks!";
-        //    } else if (enumName == DiscardGames.SixNimmt)
-        //    {
-        //        return "6 Nimmt!";
-        //    }
-
-        //    return "GAMENOTFOUND";
-        //}
-
-        // make sure you null check! if the game is not found the returned file will be null
-        public static string enumToFilePath(DiscardGames enumName)
-        {
-            if (enumName == DiscardGames.IncanGold)
-            {
-                return "incangold.jpg";
-            }
-            else if (enumName == DiscardGames.NoThanks)
-            {
-                return "nothanks.jpg";
-            }
-            else if (enumName == DiscardGames.SixNimmt)
-            {
-                return "sixnimmt.jpg";
-            }
-
-            return null;
-        }
-
-        public static DiscardGames findGameFromString(string toFind)
-        {
+        public static DiscardGames findGameFromString(string toFind) {
             toFind = toFind.Trim().ToLower().Replace(" ", string.Empty);
             Console.WriteLine("toFind looks like:\n" + toFind);
-            if (toFind == "nothanks" || toFind == "nothanks!")
-            {
+            if (toFind == "nothanks" || toFind == "nothanks!") {
                 return DiscardGames.NoThanks;
-            } else if (toFind == "6nimmt" || toFind == "sixnimmt" || 
-                toFind == "sechsnimmt" || toFind == "6nimmt!" || toFind == "sixnimmt!" || toFind == "sechsnimimt!")
-            {
-                return DiscardGames.SixNimmt;
-            } else if (toFind == "incangold" || toFind == "incangold!")
-            {
-                return DiscardGames.IncanGold;
             }
             return DiscardGames.NOTFOUND;
-        }
-
-        private Task ReactionAdder(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel channel, SocketReaction reaction)
-        {
-            // This is a reaction we added ourselves. Ignore it.
-            if (reaction.UserId == _client.CurrentUser.Id) {
-                Console.WriteLine("We reacted just now.");
-                return Task.CompletedTask;
-            }
-
-            Console.WriteLine("Looking for message just reacted");
-            if (reaction.Message.IsSpecified)
-            {
-                Console.WriteLine("Message is specified");
-                SocketUserMessage msg = reaction.Message.Value;
-                Console.WriteLine("message: " + msg.Content);
-                
-                // super clunky to hardcode like this, set up a state for the game
-                if (msg.Author.Id == _client.CurrentUser.Id && msg.Content == "Do you really want to kill this game?\n" +
-                        "🗑️ to confirm deletion. 👎 to keep playing.")
-                {
-                    Console.WriteLine("On a kill message");
-                    if (reaction.Emote.Name == new Emoji("🗑️").Name || reaction.Emote.Name == new Emoji("🗑").Name)
-                    {
-                        channel.SendMessageAsync($"Game of ***{activeGames[channel.Id].activeGame.Name}*** deleted 🗑️");
-                        activeGames.Remove(channel.Id);
-                    } else
-                    {
-                        channel.SendMessageAsync("We'll keep playing ***{activeGames[channel.Id].Name}*** 😃");
-                        channel.DeleteMessageAsync(msg.Id);
-                    }
-                    channel.DeleteMessageAsync(msg.Id);
-                } else
-                {
-                    Console.WriteLine("Hitting the map now with react by " + reaction.User.Value.Username);
-                    activeGames[channel.Id].messageMap[msg.Id](msg, reaction.User.Value);
-                }
-            }
-            return Task.CompletedTask;
-        }
-
-        private Task ReactionRemover(Cacheable<IUserMessage, ulong> cache, ISocketMessageChannel channel, SocketReaction reaction)
-        {
-
-            return Task.CompletedTask;
-        }
-
-            private Task MessageHandler(SocketMessage message)
-        {
-            if (!message.Content.StartsWith('!'))
-            {
-                return Task.CompletedTask;
-            }
-            if (message.Author.IsBot)
-            {
-                return Task.CompletedTask;
-            }
-
-            return RouteMessage(message.Content.Substring(1).Trim(), message);
         }
     }
 }
